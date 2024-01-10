36b72fba988e1edaa3965b91c96bd1eeb3c37f95
bhaskrr <74664799+bhaskrr@users.noreply.github.com>
2024-01-11 00:08:01 +0530

    Added the restart modal

--- game.js
+++ game.js
@@ -1,15 +1,22 @@
+const heading = document.querySelector("h2");
+const dialog = document.querySelector('dialog');
+const restartBtn = document.querySelector('.restart-btn');
+const gridBtns = document.querySelectorAll(".board-grid button");
+const output = document.querySelector(".output");
+
 //the board
 const board = new Array(9).fill(null);
 
-const drawBoard = () => {
-    console.clear();
-    console.log(`${board[0]} | ${board[1]} | ${board[2]} \n------------------\n
-${board[3]} | ${board[4]} | ${board[5]} \n------------------\n
-${board[6]} | ${board[7]} | ${board[8]}`);
-}
+const displayBoard = () => {
+    for (let i = 0; i < gridBtns.length; i++) {
+        gridBtns[i].textContent = board[i];
+    }
+};
+
+displayBoard(); //intial render
 
 //to store the choices
-let playerChoice;
+let playerChoice = null;
 let computerChoice;
 
 //to keep track of who's turn it is
@@ -19,43 +26,46 @@ let currentPlayer;
 const handleTurn = (position) => {
     if (board[position] === null) {
         board[position] = currentPlayer;
-        drawBoard();
+        console.table(board);
+        displayBoard();
 
-        if(checkWin()){
-            console.log(`${currentPlayer} wins!`);
+        if (checkWin()) {
+            output.textContent = `${currentPlayer} wins!`;
+            dialog.showModal(); //show the restart dialog
+
+        } else if (checkTie()) {
+            output.textContent = "It's a tie";
+            dialog.showModal();
         }
-        else if(checkTie()){
-            console.log("It's a tie");
-        }
-        else{
-            currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
-        }
+
+        currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
     }
-    else {
-        console.log('Cell is already occupied!');
-    }
-}
+};
 
 //handles a player turn
 const playerTurn = (playerPos) => {
-    handleTurn(playerPos, playerChoice);
-}
+    currentPlayer = playerChoice;
+    handleTurn(playerPos);
+};
 
 //handles a player turn
 const computerTurn = () => {
-    let pos = computerLogic();
-    handleTurn(pos, computerChoice);
-}
+    if(!checkWin() && !checkTie()){
+        currentPlayer = computerChoice;
+        let pos = computerLogic();
+        handleTurn(pos);
+    }
+};
 
 //gets a non occupied position
 const computerLogic = () => {
     let computerPosition = 0;
-    do{
-        computerPosition = Math.floor(Math.random() * 8);
-    }while(board[computerPosition] != null);
+    do {
+        computerPosition = Math.floor(Math.random() * 9);
+    } while (board[computerPosition] != null);
 
     return computerPosition;
-}
+};
 
 //checks for a winner by matching if the same symbols in the board
 //are in winning index position combinations
@@ -68,7 +78,7 @@ const checkWin = () => {
         [2, 4, 6],
         [2, 5, 8],
         [3, 4, 5],
-        [6, 7, 8]
+        [6, 7, 8],
     ];
 
     for (const combination of winCombinations) {
@@ -93,36 +103,47 @@ const checkWin = () => {
         }
     }
     return false;
-}
+};
 
 const checkTie = () => {
     return board.every((cell) => cell !== null);
+};
+
+//DOM handling
+const choiceBtns = document.querySelectorAll(".choice-btns button");
+choiceBtns.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+        //prevents choice switching mid game
+        if (playerChoice === null) {
+            playerChoice = e.target.getAttribute("choice");
+            currentPlayer = playerChoice;
+            heading.innerText = `Your choice: ${playerChoice}`;
+            computerChoice = playerChoice == "X" ? "O" : "X";
+        }
+    });
+});
+
+gridBtns.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+        if (playerChoice) {
+            const indexPosition = e.target.getAttribute("position");
+
+            playerTurn(indexPosition);
+            
+            computerTurn();
+        }
+    });
+});
+
+function restart(){
+    playerChoice = null;
+    board.fill(null);
+    displayBoard();
+    heading.innerText = 'Make a choice:';
+    output.textContent = '';
 }
 
-drawBoard();
-
-//the IIFE
-(function playGame(){
-    playerChoice = prompt('Enter your choice(X or O): ');
-    
-    computerChoice = playerChoice == 'X' ? 'O' : 'X';
-    while (!checkWin() && !checkTie()) {
-        const playerPosition = prompt(`Player ${playerChoice}, Enter a position(0-8): `);
-
-        if (playerPosition >= 0 && playerPosition <= 8){
-            if(board[playerPosition] === null) {
-                
-                currentPlayer = playerChoice;
-                playerTurn(playerPosition);
-
-                computerTurn();
-            }
-            else{
-                console.log('Position chosen by the player is occupied!');
-            }
-        }
-        else {
-            console.log('Invalid choice!');
-        }
-    }
-})();
+restartBtn.addEventListener('click', ()=>{
+    dialog.close(); //close the restart dialog
+    restart();
+})
